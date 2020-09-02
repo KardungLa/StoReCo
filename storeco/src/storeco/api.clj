6b17ff3315f24daeff94206b58a881552a92cf69
Schlager Daniel <daniel.schlager@sbg.ac.at>
2020-09-02 14:03:57 +0200

    cli improvements

--- storeco/src/storeco/api.clj
+++ storeco/src/storeco/api.clj
@@ -21,38 +21,39 @@
 
 (defn uuid [] (.toString (java.util.UUID/randomUUID)))
 
-(def *model* (rdf/defmodel))
+(def ^:dynamic *model* (rdf/defmodel))
 
 (def parent nil)
+
+(def xml-text "<?xml version='1.0' encoding='UTF-8'?><text xml:id=\"1\">a<p class=\"headline\" xml:id=\"2\">b<hi class=\"subheadline\" xml:id=\"3\">c</hi>inner</p><p xml:id=\"4\">e</p></text>")
 
 (defn get-config []
   (m/build-config
     (m/resource "config-defaults.edn")
     (m/file "./config-local.edn")))
 
-(def xml-text "<?xml version='1.0' encoding='UTF-8'?><text xml:id=\"1\">a<p class=\"headline\" xml:id=\"2\">b<hi class=\"subheadline\" xml:id=\"3\">c</hi>inner</p><p xml:id=\"4\">e</p></text>")
-
 (defn register-rdf-ns
   "Register a given list of rdf namespaces"
   [ns])
-
 
 (defn read-and-parse-str
   "Read and parse xml string"
   [xml]
   (->
-    (-> xml-text .getBytes java.io.ByteArrayInputStream.)
+    (-> xml .getBytes java.io.ByteArrayInputStream.)
     (xml/parse :namespace-aware false)))
 
 (defn read-and-parse-file
   "Read and parse xml file"
-  [filename]
-  (if (.exists (io/file filename))
-    (->>
-      (slurp filename)
-      (xml/parse-str))
-    nil
-    ))
+  [file]
+  (cond
+    (instance? java.io.File file) (do (prn "io") (xml/parse (io/input-stream file)))
+    :else
+    (if (.exists (io/file file))
+      (->>
+        (slurp file)
+        (xml/parse-str))
+      nil)))
 
 (defn update-parent
   "Update global variable p which holds the parent xml:id"
@@ -86,89 +87,92 @@
         (cond
           (string? o) (rdf/with-model model (rdf/model-add-triples (rdf/make-triples [[s p (rdf/rdf-literal o)]])))
           :else
-          (rdf/with-model model (rdf/model-add-triples (rdf/make-triples [[s p o]]))))))))
+          (rdf/with-model model (rdf/model-add-triples (rdf/make-triples [[s p o]])))))
+      (doseq [element triples] (let [s (nth element 0)
+                                     p (nth element 1)
+                                     o (nth element 2)]
+                                 (cond
+                                   (string? o) (rdf/with-model model (rdf/model-add-triples (rdf/make-triples [[s p (rdf/rdf-literal o)]])))
+                                   :else
+                                   (rdf/with-model model (rdf/model-add-triples (rdf/make-triples [[s p o]])))))))))
 
 (defn get-content
-"Get content of element"
-[element]
-(cond
-  (nil? element) "" ;; return empty String
-  (map? element) (first (:content element))
-  :else element))
+  "Get content of element"
+  [element]
+  (cond
+    (nil? element) "" ;; return empty String
+    (map? element) (first (:content element))
+    :else element))
 
 (defn xml->rdf [element]
-(let [ip parent]
-  (do
-    (cond
-      (sequential? element) (if (> (count element) 1)
-                              (do
-                                (def llp (str "List_" ip))
-                                (add-to-model [[[:dhplusi (keyword llp)]
-                                                [:rdf :type]
-                                                [:rdf :Seq]]] *model*)
-                                (add-to-model [[[:dhplusi (keyword llp)]
-                                                [:rdf :li]
-                                                [:dhplusi (keyword (get-id element))]]] *model*)
-                                (add-to-model [[[:dhplusi (keyword ip)]
-                                                [:ldp :contains]
-                                                [:dhplusi (keyword llp)]]] *model*)
-                                (if (string? (last element))
-                                  (do
-                                    ;; inner text, we need to create another instance
-                                    (def ie (str "i" (get-id element)))
-                                    (add-to-model [[[:dhplusi (keyword llp)]
-                                                    [:rdf :li]
-                                                    [:dhplusi (keyword ie)]]] *model*)
-                                    (update-parent (get-id element))
-                                    (add-to-model [[[:dhplusi (keyword ie)]
-                                                    [:dhplus :hasContent]
-                                                    (last element)]] *model*)
-                                    (add-to-model [[[:dhplusi (keyword ie)]
-                                                    [:rdf :type]
-                                                    [:dhplus :tag]]] *model*)))
-                                (update-parent (get-id element))
-                                (map xml->rdf element))
-                              (do (update-parent (get-id element))
-                                        ;(add-to-model [:dhplusi (keyword (get-id element))]
-                                        ;  [:dhplus :hasContent]
-                                        ;  (first element) *model**)
-                                  (xml->rdf (first element))))
-      (map? element) (do
-                       (update-parent (get-id element))
-                       (add-to-model [[[:dhplusi (keyword (get-id element))]
-                                       [:rdf :type]
-                                       [:dhplus :tag]]] *model*)
-                       (add-to-model [[[:dhplusi (keyword (get-id element))]
-                                       [:dhplus :elementName]
-                                       [:dhplus (name (:tag element))]]] *model*)
-                       (add-to-model [[[:dhplusi (keyword (get-id element))]
-                                       [:dhplus :hasContent]
-                                       (get-content (first (:content element)))]] *model*)
-                       (if (:attrs element)
-                         (do
-                           (if (> (count (:attrs element)) 1)
-                             (doseq [[k v] (:attrs element)]
-                               (def nid (keyword (str (get-id element) "/" (name k))))
-                               (add-to-model [[[:dhplusi (keyword (get-id element))]
-                                               [:dhplus :attrs]
-                                               [:dhplusi nid]]] *model*)
-                               (add-to-model [[[:dhplusi nid]
-                                               [:dhplus :attrName]
-                                               (name k)]] *model*)
-                               (add-to-model [[[:dhplusi nid]
-                                               [:dhplus :attrValue]
-                                               v]] *model*)))))
-                       (xml->rdf (:content element)))))))
+  (let [ip parent]
+    (do
+      (cond
+        (sequential? element) (if (> (count element) 1)
+                                (do
+                                  (def llp (str "List_" ip))
+                                  (add-to-model
+                                    [[[:dhplusi (keyword llp)]
+                                      [:rdf :type]
+                                      [:rdf :Seq]]
+                                     [[:dhplusi (keyword ip)]
+                                      [:ldp :contains]
+                                      [:dhplusi (keyword llp)]]] *model*)
+                                  (if (string? (last element))
+                                    (do
+                                      ;; inner text, we need to create another instance
+                                      (def ie (str "i" (get-id element)))
+                                      (add-to-model [[[:dhplusi (keyword ie)]
+                                                      [:dhplus :hasContent]
+                                                      (last element)]
+                                                     [[:dhplusi (keyword ie)]
+                                                      [:rdf :type]
+                                                      [:dhplus :tag]]] *model*)))
+                                  (update-parent (get-id element))
+                                  (map xml->rdf element))
+                                (do (update-parent (get-id element))
+                                    (xml->rdf (first element))))
+        (map? element) (do
+                         (update-parent (get-id element))
+                         (add-to-model [[[:dhplusi (keyword (get-id element))]
+                                         [:rdf :type]
+                                         [:dhplus :tag]]
+                                        [[:dhplusi (keyword (get-id element))]
+                                         [:dhplus :elementName]
+                                         [:dhplus (name (:tag element))]]
+                                        [[:dhplusi (keyword (get-id element))]
+                                         [:dhplus :hasContent]
+                                         (get-content (first (:content element)))]] *model*)
+                         (if (:attrs element)
+                           (do
+                             (if (> (count (:attrs element)) 1)
+                               (doseq [[k v] (:attrs element)]
+                                 (def nid (keyword (str (get-id element) "/" (name k))))
+                                 (add-to-model [[[:dhplusi (keyword (get-id element))]
+                                                 [:dhplus :attrs]
+                                                 [:dhplusi nid]]
+                                                [[:dhplusi nid]
+                                                 [:dhplus :attrName]
+                                                 (name k)]
+                                                [[:dhplusi nid]
+                                                 [:dhplus :attrValue]
+                                                 v]] *model*)))))
+                         (xml->rdf (:content element)))))))
 
 (defn build
   "Build RDF from XML and save"
-  [options]
-  (let [document (read-and-parse-file (:input options))]
-    (cond
-      (nil? document) (prn "File not found")
-      :else
-      (do
-        (def *model* (rdf/defmodel))
-        (xml->rdf document)
-        (def rdf-str (with-out-str (rdf/model-to-format *model* (keyword (:format options)))))
-        (spit (:output options) rdf-str)))))
+  ([options]
+   (prn options)
+   (let [document (read-and-parse-file (:input options))
+         output (:output options)
+         format (:format options)]
+     (cond
+       (nil? document) (prn "File not found")
+       :else
+       (spit output (with-out-str (rdf/model-to-format *model* (keyword format)) (xml->rdf document))))))
+  ([input output format]
+   (let [document (read-and-parse-file input)]
+     (cond
+       (nil? document) (prn "File not found")
+       :else
+       (spit output (with-out-str (rdf/model-to-format *model* (keyword format)) (xml->rdf document)))))))
--- storeco/src/storeco/core.clj
+++ storeco/src/storeco/core.clj
@@ -13,9 +13,9 @@
 (def cli-opts
   [["-h" "--help"]
    ["-c" "--config PATH" "EDN file to read config options from"]
-   ["-i" "--input INPUT"]
+   ["-i" "--input INPUT" :parse-fn #(java.io.File. %) :validate [#(.exists %) "file must exist"]]
    ["-o" "--output OUTPUT"]
-   ["-of" "--output-format FORMAT"]]
+   ["-f" "--format FORMAT"]]
   )
 
 (defn help-text [options-summary]
@@ -27,13 +27,14 @@
 " options-summary))
 
 (defn -main [& args]
-  (let [{:keys [options summary errors]} (cli/parse-opts args cli-opts :no-defaults true)
+  (let [{:keys [options errors summary]} (cli/parse-opts args cli-opts :no-defaults true)
         {:keys [help]} options
         config-file (api/get-config)
         options (merge (cli/get-default-options cli-opts)
                   config-file
                   (dissoc options :config))]
     (cond
+      (nil? args) (println (help-text summary))
       errors (println (str/join "\n" errors))
       help (println (help-text summary))
       :else (api/build options))))
